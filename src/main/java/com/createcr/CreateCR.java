3349f612e7f3cfee914c2d603f3c9634133b8e17
Cabbage698 <jesusgomez03178@gmail.com>
2026-05-31 22:41:44 -0600

    Add more things

--- src/main/java/com/createcr/CreateCR.java
+++ src/main/java/com/createcr/CreateCR.java
@@ -1,6 +1,5 @@
 package com.createcr;
 
-import com.createcr.item.ModItems;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
@@ -9,6 +8,5 @@ public class CreateCR {
     public static final String MOD_ID = "createcr";
 
     public CreateCR(IEventBus modEventBus) {
-        ModItems.ITEMS.register(modEventBus);
     }
 }
